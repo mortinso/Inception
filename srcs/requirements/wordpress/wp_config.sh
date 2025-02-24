7db17e7980a6d95b658e575d4766f4f2feef1b80
mortinso <89762267+mortinso@users.noreply.github.com>
2025-02-24 18:44:25 +0000

    Wordpress CLI script thingy.

--- srcs/requirements/wordpress/wp_config.sh
+++ srcs/requirements/wordpress/wp_config.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
+# Creates a new wp-config.php with database constants
+create_config() {
+	wp config create \
+	--path=/var/www/wordpress \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 \
+	--allow-root \
+	--force
+}
+
+# Creates the WordPress tables in the database using the URL, title, and default admin user details provided
+install() {
+	wp core install \
+	--allow-root \
+	--url=$WP_URL/ \
+	--title=$WP_TITLE \
+	--admin_user=$WP_ADMIN_LOGIN \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL
+}
+
+# Creates a new user
+create_user() {
+	wp user create \
+	--allow-root \
+	$WP_USER_LOGIN \
+	$WP_USER_EMAIL \
+	--user_pass: $WP_USER_PASSWORD \
+	--role=author
+}
+
 if [ ! -f wp-config.php ]
 then
+	# Download core wordpress files
+	wp core download --allow-root
+	create_config
+	install
+	create_user
+
+	wp option update home "https://$WP_URL" --allow-root
+	wp option update siteurl "https://$WP_URL" --allow-root
 
 else
 	echo "Wordpress is already installed and set up."
